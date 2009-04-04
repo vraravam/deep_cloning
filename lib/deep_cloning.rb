e28c0c3f455d7f0124d52ab60735d8aeda2ffc35
Vijay Aravamudhan <avijayr - at - gmail - dot - com>
2009-04-03 22:50:24 -0500

    Keep a local variable of the association reflection (can improve performance for VERY deep trees); also fixed a bug where the foreign key might be nil and if so fall back to the current class's underscored name

--- lib/deep_cloning.rb
+++ lib/deep_cloning.rb
@@ -44,12 +44,12 @@ module DeepCloning
           association = association.keys.first
         end
         opts = deep_associations.blank? ? {} : {:include => deep_associations}
-        fk = self.class.reflect_on_association(association).options[:foreign_key]
-       
-        cloned_object = case self.class.reflect_on_association(association).macro
+        association_reflection = self.class.reflect_on_association(association)
+        cloned_object = case association_reflection.macro
                         when :belongs_to, :has_one
                           self.send(association) && self.send(association).clone(opts)
                         when :has_many, :has_and_belongs_to_many
+                          fk = association_reflection.options[:foreign_key] || self.class.to_s.underscore
                           self.send(association).collect { |obj| tmp = obj.clone(opts)
                                                                  tmp.send("#{fk}=", kopy)
                                                                  tmp
